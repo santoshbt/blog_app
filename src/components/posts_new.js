5e6c5244352ca43d8f522fcb8219cbac8bc078a6
santoshbt <bt.santosh01@gmail.com>
2018-02-12 10:20:15 +0530

    Added redux form and CRUD operations

---
+++ src/components/posts_new.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+
+class PostsNew extends Component {
+  renderField(field){
+    const {meta : { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
+    return(
+      <div className={className}>
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}   // any event can be communicated to field (pre-generated event handlers)
+        />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
+      </div>
+    );
+  }
+
+  onSubmit(values){
+
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    } );
+  }
+
+  render(){
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+         />
+         <Field
+           label="Categories"
+           name="categories"
+           component={this.renderField}
+         />
+         <Field
+           label="Post Content"
+           name="content"
+           component={this.renderField}
+         />
+         <button type="submit" className="btn btn-primary">Submit</button>
+         <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
+    );
+  }
+}
+
+function validate(values){
+  const errors = {};
+
+  // Validate the input from 'values'
+  if(!values.title || values.title.length < 3){
+    errors.title = "Enter a title! that has atleast 3 characters!";
+  }
+
+  if(!values.categories){
+    errors.categories = "Enter some categories!";
+  }
+
+  if(!values.content){
+    errors.content = "Enter some content please!";
+  }
+  // if errors is empty, the form is fine to submit
+  // if errors has any properties, redux form asssumes form is invalid
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'        // make sure the string you assign is unique.
+})(
+    connect(null, { createPost })(PostsNew)
+);                            // This is helpful whenever we want to merge state from multiple forms
+                              // in a given time.
